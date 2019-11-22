7726e2fff58484317c1728848d0663ad2f9a8b5e
Jimmy Dickinson <jamesfdickinson@gmail.com>
2019-11-22 01:49:49 -0800

    Notification

--- src/services/Notification.js
+++ src/services/Notification.js
@@ -6,14 +6,14 @@ export default class Notification {
         this.firebase.onMessage = function () { };
         this.firebase.onTokenRefresh = this.tokenRefresh.bind(this);
     }
-    init(){
+    init() {
         this.firebase.init();
     }
     sendTokenToServer(currentToken, userId) {
         //todo: get userId should be from centralize class or passed in on request, but it can also be called from a token refresh
         //get user Id
         if (!userId) {
-            let user = JSON.parse(localStorage.getItem("user"))||{};
+            let user = JSON.parse(localStorage.getItem("user")) || {};
             userId = user.id;
         }
         console.log('Sending token to server...');
@@ -29,22 +29,23 @@ export default class Notification {
         });
     }
     requestNotificationToken(userId) {
-        if(userId) this.userId = userId;
+        if (userId) this.userId = userId;
         console.log('Requesting permission...');
         // [START request_permission]
-        return Notification.requestPermission().then((permission) => {
-            if (permission === 'granted') {
-                console.log('Notification permission granted.');
-                return this.firebase.getToken();
+        return window.Notification.requestPermission()
+            .then((permission) => {
+                if (permission === 'granted') {
+                    console.log('Notification permission granted.');
+                    return this.firebase.getToken();
                     // .then((token) => {
                     //     return this.sendTokenToServer(token, userId).then(function () {
                     //         return token;
                     //     });
                     // });
-            } else {
-                console.log("Notification: Unable to get permission to notify.");
-                return null;
-            }
-        });
+                } else {
+                    console.log("Notification: Unable to get permission to notify.");
+                    return null;
+                }
+            });
     }
 }
