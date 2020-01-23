cd412bc4e5e44b613ee7ae806b023f5ea2e75855
Jimmy Dickinson <jamesfdickinson@gmail.com>
2020-01-22 21:07:51 -0800

    faq access

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@ router.beforeEach((to, from, next) => {
   
 
   //requirer 
-  const pagesNoAuthenticationRequired = ['UserCreate','Login','SignUp','SignUpComplete'];
+  const pagesNoAuthenticationRequired = ['UserCreate','Login','SignUp','SignUpComplete','FAQ'];
   const authRequired = !pagesNoAuthenticationRequired.includes(to.name);
   if (authRequired && !user) {
     return next(`/Login?redirect=${to.path}`);
