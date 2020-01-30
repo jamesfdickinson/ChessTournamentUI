24d253e0ffabf93be576aa62b1cb4067ee69fb46
Jimmy Dickinson <jamesfdickinson@gmail.com>
2020-01-29 17:10:27 -0800

    add assetlink

--- www/service-worker.js
+++
@@ -1,6 +0,0 @@
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
- 
-});
