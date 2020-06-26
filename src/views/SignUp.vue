2abf5285f415557c4cc9286b1f57b1a6b3df0c1a
Jimmy Dickinson <jamesfdickinson@gmail.com>
2020-06-26 14:18:29 -0700

    only allow numbers for sign up

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -40,8 +40,8 @@
             <ion-input :value="player.lastName" @input="player.lastName = $event.target.value"></ion-input>
             </ion-item>-->
             <ion-item>
-              <ion-label position="stacked">Cribbage ID (Invite Code)</ion-label>
-              <ion-input :value="player.gamerId" @input="player.gamerId = $event.target.value"></ion-input>
+              <ion-label position="stacked">Cribbage ID</ion-label>
+              <ion-input type="number" :value="player.gamerId" @input="player.gamerId = $event.target.value"></ion-input>
             </ion-item>
             <ion-item>
               <ion-label position="stacked">Email</ion-label>
