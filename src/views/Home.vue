b0e0fd0679879cdbe80741a17d3c5628005533b6
Jimmy Dickinson <jamesfdickinson@gmail.com>
2023-12-08 16:54:48 -0800

    version 1.0.2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,7 +52,7 @@
           <footer>
             <p>
               <a href="https://bracketjd.com">Bracket JD</a> |
-              <a href="mailto:tournament@jdsoftwarellc.com">Contact</a> | <a href="">version 1.0.1</a>
+              <a href="mailto:tournament@jdsoftwarellc.com">Contact</a> | <a href="">version 1.0.2</a>
        
             </p>
           </footer>
