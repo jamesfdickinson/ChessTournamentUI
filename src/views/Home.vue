e52dffb2e475087fd3bfea9df4240aa893be21d0
Jimmy Dickinson <jamesfdickinson@gmail.com>
2024-03-22 03:41:20 -0700

    11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -58,7 +58,7 @@
           <footer>
             <p>
               <a href="https://bracketjd.com">Bracket JD</a> |
-              <a href="mailto:tournament@jdsoftwarellc.com">Contact</a> | <a href="">version 1.0.10</a>
+              <a href="mailto:tournament@jdsoftwarellc.com">Contact</a> | <a href="">version 1.0.11</a>
 
             </p>
           </footer>
