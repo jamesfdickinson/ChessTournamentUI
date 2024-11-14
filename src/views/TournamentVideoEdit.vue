62c9b9c2f4ac6698592d172369421c0e0fe9a805
Jimmy Dickinson <jamesfdickinson@gmail.com>
2024-11-14 04:08:37 -0800

    youtube stream

--- src/views/TournamentVideoEdit.vue
+++ src/views/TournamentVideoEdit.vue
@@ -4,11 +4,7 @@
     <ion-header>
       <ion-toolbar color="primary">
         <ion-buttons slot="start">
-          <ion-icon
-            name="arrow-round-back"
-            size="large"
-            @click="$router.go(-1)"
-          ></ion-icon>
+          <ion-icon name="arrow-round-back" size="large" @click="$router.go(-1)"></ion-icon>
         </ion-buttons>
         <ion-title>Edit Video</ion-title>
       </ion-toolbar>
@@ -22,19 +18,18 @@
           <ion-list>
             <ion-item>
               <ion-label position="stacked">Twitch username</ion-label>
-              <ion-input
-                placeholder="twitchName"
-                :value="twitchName"
-                @input="twitchName = $event.target.value"
-              ></ion-input>
+              <ion-input placeholder="twitchName" :value="twitchName"
+                @input="twitchName = $event.target.value"></ion-input>
+            </ion-item>
+            <ion-item>
+              <ion-label position="stacked">YouTube Channel ID</ion-label>
+              <ion-input placeholder="YouTube Channel Id" :value="youtubeChannelId"
+                @input="youtubeChannelId = $event.target.value"></ion-input>
             </ion-item>
             <ion-item>
               <ion-label position="stacked">Subscribe link</ion-label>
-              <ion-input
-                placeholder="https://somelink.com/321f4"
-                :value="subscribeURL"
-                @input="subscribeURL = $event.target.value"
-              ></ion-input>
+              <ion-input placeholder="https://somelink.com/321f4" :value="subscribeURL"
+                @input="subscribeURL = $event.target.value"></ion-input>
             </ion-item>
           </ion-list>
           <ion-button v-on:click="generate()">Generate</ion-button>
@@ -45,24 +40,29 @@
           <ion-label>Raw HTML</ion-label>
         </ion-list-header>
         <ion-item>
-          <ion-textarea
-            rows="6"
-            cols="20"
-            :value="tournament.video"
-            @input="tournament.video = $event.target.value"
-          ></ion-textarea>
+          <ion-textarea rows="6" cols="20" :value="tournament.video"
+            @input="tournament.video = $event.target.value"></ion-textarea>
         </ion-item>
       </ion-list>
 
       <div style="padding: 15px 15px">
         <ion-button expand="block" v-on:click="save()">Save</ion-button>
         <hr />
-        <ion-button expand="block" color="light" v-on:click="back()"
-          >Cancel</ion-button
-        >
+        <ion-button expand="block" color="light" v-on:click="back()">Cancel</ion-button>
 
         <div style="color: red">{{ error }}</div>
       </div>
+      <ion-list>
+        <ion-list-header>
+          Help
+        </ion-list-header>
+        <ion-item> <a href="https://support.google.com/youtube/answer/3250431?hl=en"
+          target="_blank">How to find YouTube Channel ID </a>
+        </ion-item> 
+        <ion-item> <a href="https://www.youtube.com/account_advanced"
+          target="_blank">YouTube Channel ID link</a>
+        </ion-item>   
+      </ion-list>
     </ion-content>
     <!-- </ion-page> -->
   </layout-menu>
@@ -92,6 +92,7 @@ export default {
         type: null,
       },
       twitchName: "",
+      youtubeChannelId: "",
       subscribeURL: "",
       error: "",
     };
@@ -103,6 +104,7 @@ export default {
     generate() {
       let html = "";
       let twitchName = this.twitchName;
+      let youtubeChannelId = this.youtubeChannelId;
       let subscribeURL = this.subscribeURL;
       if (twitchName) {
         html += `<iframe
@@ -112,10 +114,16 @@ export default {
               allowfullscreen="true">
           </iframe>`;
       }
+      if (youtubeChannelId) {
+        html += `<iframe width="100%" height="200" src="https://www.youtube.com/embed/live_stream?channel=${youtubeChannelId}" ;autoplay=1" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+      }
+      // if (facebookPageId) {
+      //   html += `<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FCribbageJD%2Fvideos%2F488266577552838%2F&width=480" width="480" height="360" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>`;
+      // }
       if (subscribeURL) {
         html += `<br>
           <a href="${subscribeURL}" style="padding: 10px 12px;margin:4px 4px;font-size: 16px;text-align: center;text-decoration: none;color: white;display: inline-block;border-radius: 4px;background-color: #008CBA;"> Subscribe</a> Get notified for upcoming tournaments`;
-          // <a href="${subscribeURL}">${subscribeURL}</a>
+        // <a href="${subscribeURL}">${subscribeURL}</a>
       }
       if (html) {
         this.tournament.video = html;
