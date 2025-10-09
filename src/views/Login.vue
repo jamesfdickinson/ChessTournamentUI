993d6bb19b1864f9577c4ba98736f584b22e9090
Jimmy Dickinson <jamesfdickinson@gmail.com>
2025-10-08 23:18:22 -0700

    cross domain login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,11 +4,7 @@
     <ion-header>
       <ion-toolbar color="primary">
         <ion-buttons slot="start">
-          <ion-icon
-            name="arrow-round-back"
-            size="large"
-            @click="$router.go(-1)"
-          ></ion-icon>
+          <ion-icon name="arrow-round-back" size="large" @click="$router.go(-1)"></ion-icon>
         </ion-buttons>
         <ion-title>Login</ion-title>
       </ion-toolbar>
@@ -23,29 +19,19 @@
               </div>
               <div padding>
                 <ion-item>
-                  <ion-input
-                    type="email"
-                    placeholder="Email"
-                    :value="userName"
-                    @input="userName = $event.target.value"
-                  ></ion-input>
+                  <ion-input type="email" placeholder="Email" :value="userName"
+                    @input="userName = $event.target.value"></ion-input>
                 </ion-item>
 
                 <ion-item>
-                  <ion-input
-                    type="password"
-                    placeholder="Password"
-                    :value="password"
-                    @input="password = $event.target.value"
-                  ></ion-input>
+                  <ion-input type="password" placeholder="Password" :value="password"
+                    @input="password = $event.target.value"></ion-input>
                 </ion-item>
               </div>
               <!-- Needs a sumbit button so the form will submit on enter. <input type="submit"> or <button>(defaults to submit) will work. The ion-button is in the shadow dom and the form does not see it !-->
               <input type="submit" style="display: none" />
               <div padding>
-                <ion-button type="submit" size="large" expand="block"
-                  >Login</ion-button
-                >
+                <ion-button type="submit" size="large" expand="block">Login</ion-button>
                 <ul style="color: red">
                   <li v-for="error in errors" v-bind:key="error">
                     *{{ error }}
@@ -59,13 +45,15 @@
 
       <div padding style="text-align: center; margin-top: 15px">
         <p>Need an account?</p>
-        <ion-button type="button" size="large" expand v-on:click="openSignUp()"
-          >Sign up</ion-button
-        >
+        <ion-button type="button" size="large" expand v-on:click="openSignUp()">Sign up</ion-button>
       </div>
       <div padding style="text-align: center; margin-top: 15px">
         <p><a v-on:click="openPasswordReset()"> Forgot password?</a></p>
       </div>
+
+      <!-- Hidden iframe for cross-domain localStorage access -->
+      <iframe ref="bracketjdIframe" :src="iframeUrl" style="xdisplay: none;"></iframe>
+
     </ion-content>
     <!-- </ion-page> -->
   </layout-no-menu>
@@ -98,6 +86,7 @@ export default {
       password: "",
       redirect: redirect,
       errors: [],
+      iframeUrl: 'https://bracketjd.com/cross-domain-helper.html',
     };
   },
   methods: {
@@ -153,7 +142,60 @@ export default {
           console.warn(e);
         });
     },
+
+    //todo: remove after a few months - 10/8/2025
+    //-------------Cross domain user data pull------------------//
+    //--window.addEventListener('message', this.handleCrossDomainMessage);
+    //--window.removeEventListener('message', this.handleCrossDomainMessage);
+
+  
+    handleCrossDomainMessage(event) {
+      // Only accept messages from trusted origins
+      const trustedOrigins = [
+        'http://localhost:8080',
+        'https://localhost:8080',
+        'https://bracketjd.com'
+      ];
+      
+      if (!trustedOrigins.includes(event.origin)) {
+        console.warn('Message from untrusted origin:', event.origin);
+        return;
+      }
+
+      console.log('Received message from iframe:', event.data);
+
+      if (event.data.action === 'localStorageData' && event.data.key === 'user') {
+        const crossDomainUser = event.data.value;
+        console.log('Cross-domain user data received:', crossDomainUser);
+
+        // If this was automatically sent and we have a redirect, attempt auto-login
+        if (event.data.autoSent && this.redirect && crossDomainUser) {
+          console.log('Auto-sent data received with redirect - attempting cross-site login');
+          this.attemptCrossSiteLogin(crossDomainUser);
+        }
+      }
+    },
+    attemptCrossSiteLogin(userdata) {
+      if (userdata) {
+        localStorage.setItem("user", JSON.stringify(userdata));
+        notification.requestNotificationToken();
+        notificationSocket.reconnect(user.token);
+        if (redirect) {
+          this.$router.push({ path: redirect });
+        } else {
+          this.$router.push({ path: `/` });
+        }
+      }
+    },
+    //-------------end------------------//
   },
-  created() {},
+  created() {
+    // Always listen for cross-domain messages - the iframe will send data when ready
+    window.addEventListener('message', this.handleCrossDomainMessage);
+  },
+  beforeDestroy() {
+    // Clean up event listener
+    window.removeEventListener('message', this.handleCrossDomainMessage);
+  },
 };
 </script>
