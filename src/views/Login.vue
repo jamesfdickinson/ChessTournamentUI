36a0569e69f73c50ad596f0af4add78f4be6cae5
Jimmy Dickinson <jamesfdickinson@gmail.com>
2025-10-09 17:46:21 -0700

    Redirect to bracketjd.com's auth-callback page which will extract the token and redirect back

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -161,8 +161,8 @@ export default {
       const returnUrl = `${currentUrl}/Login`;
       const redirectParam = this.redirect ? `&redirect=${encodeURIComponent(this.redirect)}` : '';
 
-      // Redirect to backetjd.com's auth-callback page which will extract the token and redirect back
-      window.location.href = `https://backetjd.com/statichtml/auth-callback.html?returnUrl=${encodeURIComponent(returnUrl)}${redirectParam}`;
+      // Redirect to bracketjd.com's auth-callback page which will extract the token and redirect back
+      window.location.href = `https://bracketjd.com/statichtml/auth-callback.html?returnUrl=${encodeURIComponent(returnUrl)}${redirectParam}`;
     },
     handleTokenCallback() {
       // If we have a token in the URL, attempt to use it for authentication
